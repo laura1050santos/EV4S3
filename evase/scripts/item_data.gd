7b0ce0fd69609d7bb0a7876312ea703f71fe5b15
laura1050santos <64499700+laura1050santos@users.noreply.github.com>
2026-04-21 20:17:30 -0300

    Delete evase directory

--- evase/scripts/item_data.gd
+++
@@ -1,5 +0,0 @@
-extends Resource
-class_name itemData
-@export var item_name: String
-@export var icon: Texture2D
-@export var mesh: Texture2D
--- evase/scripts/porta.gd
+++
@@ -1,6 +0,0 @@
-extends Sprite2D
-
-func _input_event(_viewport, event, _shape_idx) -> void:
-	if event.is_action_pressed("click"):
-		print("clicou")
-		get_tree().change_scene_to_file("res://scenes/zoom.tscn")
