119e4b44c3fac80499d8c2d35ca2e330e60a050c
laura1050santos <64499700+laura1050santos@users.noreply.github.com>
2026-05-02 00:15:44 -0300

    Add files via upload

--- scenes/start.gd
+++ scenes/start.gd
@@ -1,8 +1,11 @@
 extends Control
 # No script da sua CENA (ex: Level1.gd)
-
-
+@onready var canvas = Inventario.get_node("canvasLayer")
+func _ready() -> void:
+	canvas.hide()
+	
 func _on_jogar_pressed():
+	canvas.show()
 	get_tree().change_scene_to_file("res://scenes/ParedeNorte.tscn")
 
 func _on_sair_pressed():
