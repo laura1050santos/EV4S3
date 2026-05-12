3f0e2fcf0be86b1785b628dbd3035626644db307
laura1050santos <64499700+laura1050santos@users.noreply.github.com>
2026-05-12 00:30:50 -0300

    scrr mds
    add lanterna e arrumando sprites de cena

--- scripts/salas_scripts/parede_leste.gd
+++ scripts/salas_scripts/parede_leste.gd
@@ -4,7 +4,16 @@ func _ready():
 	var nome_desta_cena = self.name # O nome do nó raiz desta cena
 
 	var itens_iniciais=[
-		#itens que começam na cena
+
+{
+
+"item": preload("res://recursos/lanterna.tres"),
+
+"pos": Vector2(500, 550),
+
+"cena": nome_desta_cena
+
+},#itens que começam na cena
 	
 	]
 	iniciar_itens_cena(nome_desta_cena, itens_iniciais)
