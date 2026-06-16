f52777c4d46a3659b43b996cb690a3549bf36be6
Any Gabriely <any.moraes@aluno.ifsp.edu.br>
2026-06-16 20:33:14 -0300

    gaveta1 abrindo e fechando

--- scripts/salas_scripts/Global_singleton.gd
+++ scripts/salas_scripts/Global_singleton.gd
@@ -2,6 +2,8 @@ extends Node
 
 var itens_no_mundo = []
 var cenas_visitadas =[]
+
+var sprite_visivel: bool = true # teste da gaveta do aquario
 # Função para registrar um item novo no mundo
 func registrar_item(item_resource, posicao,nome_da_cena):
 	
