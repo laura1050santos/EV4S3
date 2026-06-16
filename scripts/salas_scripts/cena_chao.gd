824023be00a6e911154adcda0d0eb641bf8cbb8f
Sol Aniel <solaaniel@gmail.com>
2026-06-16 00:20:09 -0300

    Fluxo de cenas e transições correto

--- scripts/salas_scripts/cena_chao.gd
+++ scripts/salas_scripts/cena_chao.gd
@@ -1,12 +1,6 @@
 extends "res://scripts/salas_scripts/salas_manager.gd"
-
 func _ready():
 	var nome_desta_cena = self.name # O nome do nó raiz desta cena
-	
-	var itens_iniciais=[
-		#itens que começam na cena
-	
-	]
-	iniciar_itens_cena(nome_desta_cena, itens_iniciais)
 
-	
+	var itens_iniciais=[]
+	iniciar_itens_cena(nome_desta_cena, itens_iniciais)
