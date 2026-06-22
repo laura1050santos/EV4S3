a232d91e4ade79d484df2c367aff640132628a81
laura1050santos <64499700+laura1050santos@users.noreply.github.com>
2026-06-22 19:26:30 -0300

    Enhance item activation and spawning in salas_manager
    Added functionality to activate items when reconstructed and improved item spawning logic.

--- scripts/salas_scripts/salas_manager.gd
+++ scripts/salas_scripts/salas_manager.gd
@@ -1,5 +1,7 @@
 extends Node2D
+# salas_manager.gd
 
+	
 func iniciar_itens_cena(nome_desta_cena, itens_inciais):
 	if not GlobalSingleton.cena_ja_foi_registrada(nome_desta_cena):
 		for info in itens_inciais:
@@ -9,6 +11,7 @@ func iniciar_itens_cena(nome_desta_cena, itens_inciais):
 			
 	for info in GlobalSingleton.itens_no_mundo:
 		if info.cena == nome_desta_cena:
+			#precisa saber se o item estava ativo
 			spawnar_itens(info.data, info.pos)
 			
 	GlobalSingleton.registrar_transicao(scene_file_path)
@@ -20,17 +23,26 @@ func spawnar_itens(recurso, posicao):
 	var node = preload("res://scenes/inventario/worldItem.tscn").instantiate()
 	node.set_meta("item_data", recurso)
 	
-	if recurso.get("item_ativo") == true and recurso.ativo_icon:
+	if recurso.item_ativo:
 		node.texture = recurso.ativo_icon
 	else:
 		node.texture = recurso.icon 
 	
 	add_child(node)
 	node.global_position = posicao
+	ativar_item_ao_resconstruir(recurso,node)
+
+	
+func ativar_item_ao_resconstruir(item,node):
+	match item.item_name:
+		"lanterna":
+			itemData.ativar_luz(
+				item, node,node.global_position
+			)
 
 # Qualquer um pode chamar essa função para dropar/criar um item nesta sala
 func adicionar_item_na_sala(recurso, posicao):
-	# 1. Salva no Singleton com o nome desta sala
+	
 	GlobalSingleton.registrar_item(recurso, posicao, self.name)
 	# 2. Instancia fisicamente na tela
 	spawnar_itens(recurso, posicao)
