c06e3b896a4aee56883b0e20acd1eafcf1acf249
laura1050santos <64499700+laura1050santos@users.noreply.github.com>
2026-06-22 22:59:19 -0300

    mecanic Teto_lampada

--- scripts/salas_scripts/cena_teto.gd
+++ scripts/salas_scripts/cena_teto.gd
@@ -3,21 +3,26 @@ func _ready():
 	var nome_desta_cena = self.name # O nome do nó raiz desta cena
 
 	var itens_iniciais=[
-
+{
+"item": preload("res://recursos/LampadaQuebrada.tres"),
+"pos": Vector2(575, 275),
+"cena": nome_desta_cena
+},
 {
 "item": preload("res://recursos/flan.tres"),
-"pos": Vector2(570, 380),
-
+"pos": Vector2(575, 275),
 "cena": nome_desta_cena
 },
 #itens que começam na cena
 ]
 	iniciar_itens_cena(nome_desta_cena, itens_iniciais)
 	
+
 func _on_area_lampada_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	var lampada = get_node_or_null("LampadaNova")
-	if lampada:
-		lampada.position = Vector2(575,250)
-		print(lampada, "interruptor deve funcionar agr")
-	else:
-		print("lampada não encontrada")#ativar o interruptor
+	if has_node("LampadaNova") and !has_node("Quebrada"):
+		var lampadaNova = get_node("LampadaNova")
+		lampadaNova.position= Vector2(575, 275)
+		print("lampada trocada")
+		
+	
+		 # Replace with function body.
