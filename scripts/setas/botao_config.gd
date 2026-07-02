88e1f7e8b32350cddf8b2508d22303c69512354f
sara <sarammartinsoliveira@gmail.com>
2026-07-02 20:52:58 -0300

    Pequenas alterações

--- scripts/setas/botao_config.gd
+++ scripts/setas/botao_config.gd
@@ -1,1 +1,14 @@
 extends Node2D
+<<<<<<< Updated upstream
+=======
+@onready var configMenu = preload("res://scenes/telaInicial/configuracao.tscn")
+
+func _on_button_pressed() -> void:
+	var menu = get_node_or_null("menu")
+	if not menu :
+		print(menu)
+		menu = configMenu.instantiate()
+		add_child(menu)
+	 
+	# Replace with function body.
+>>>>>>> Stashed changes
