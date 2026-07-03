c5d7f5e0922e6b5fc7498c9af54602c31d27e15f
laura1050santos <64499700+laura1050santos@users.noreply.github.com>
2026-07-02 21:19:05 -0300

    objeto aparece ao  quebrar aquario

---
+++ scenes/fase1/viewCartaRelogio.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			$"../Node/carta_relogio2".visible = true
