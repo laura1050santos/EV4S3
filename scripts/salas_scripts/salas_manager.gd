e2798452121c1449b0f82f11f40fb93b22b33daa
Any Gabriely <any.moraes@aluno.ifsp.edu.br>
2026-07-28 20:38:50 -0300

    sara se redimindob depois se fuder com a vida da any

--- scripts/salas_scripts/salas_manager.gd
+++ scripts/salas_scripts/salas_manager.gd
@@ -19,8 +19,12 @@ func iniciar_itens_cena(nome_desta_cena, itens_inciais):
 	#GlobalSingleton.registrar_transicao(scene_file_path) travou de rodar com esse erro:
 	# Invalid call. Nonexistent function 'registrar_transicao' in base 'Node (Global_singleton.gd)'. 
 
-func spawnar_itens(recurso, posicao):
+func spawnar_itens(caminho_recurso, posicao):
+	
+	var recurso = load(caminho_recurso)
+	
 	var node = preload("res://scenes/inventario/worldItem.tscn").instantiate()
+	
 	node.set_meta("item_data", recurso)
 	
 	if recurso.item_ativo:
@@ -30,6 +34,7 @@ func spawnar_itens(recurso, posicao):
 	
 	add_child(node)
 	node.global_position = posicao
+	
 	ativar_item_ao_resconstruir(recurso,node)
 
 	
