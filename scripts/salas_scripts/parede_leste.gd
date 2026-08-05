fc87dc5f060e9cd05a33e9912eba1feacbc649b4
sara martins <sarammartinsoliveira@gmail.com>
2026-08-04 22:00:23 -0300

    Gavetas Pc

--- scripts/salas_scripts/parede_leste.gd
+++ scripts/salas_scripts/parede_leste.gd
@@ -19,15 +19,32 @@ func _ready():
 ]
 	iniciar_itens_cena(nome_desta_cena, itens_iniciais)
 
-	
-	
+@onready var gaveta1_sprite = $Gaveta1Pc
+@onready var gaveta2_sprite = $Gaveta2Pc
+@onready var gaveta3_sprite = $Gaveta3Pc
 
+func _on_gaveta_1_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		GlobalSingleton.gaveta_1 = !GlobalSingleton.gaveta_1
+		gaveta1_sprite.visible = GlobalSingleton.gaveta_1
+		print("Ativado 1")
+
+func _on_gaveta_2_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		GlobalSingleton.gaveta_2 = !GlobalSingleton.gaveta_2
+		gaveta2_sprite.visible = GlobalSingleton.gaveta_2
+		print("Ativado 2")
+	pass
 		
-
+func _on_gaveta_3_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		GlobalSingleton.gaveta_3 = !GlobalSingleton.gaveta_3
+		gaveta3_sprite.visible = GlobalSingleton.gaveta_3
+		print("Ativado 3")
+	pass # Replace with function body.
 
 func _on_pc_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	get_tree().change_scene_to_file("res://scenes/fase1/zoom_monitor.tscn") # Replace with function body.
-
 
 func _on_monitor_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	get_tree().change_scene_to_file("res://scenes/fase1/zoom_monitor.tscn") # Replace with function body.
