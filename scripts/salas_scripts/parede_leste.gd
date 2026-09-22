d57cf3199ab67f457c1411723db881e6f19bb4a3
sara martins <sarammartinsoliveira@gmail.com>
2026-09-22 20:15:05 -0300

    Errinho de cenas consertado (?)

--- scripts/salas_scripts/parede_leste.gd
+++ scripts/salas_scripts/parede_leste.gd
@@ -52,7 +52,7 @@ func _on_pc_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> vo
 
 func _on_monitor_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		get_tree().change_scene_to_file("res://scenes/fase1/zoom_pc.tscn") # Replace with function body.
+		get_tree().change_scene_to_file("res://scenes/fase1/zoom_monitor.tscn") # Replace with function body.
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):  # tecla Enter/Espaço por padrão
